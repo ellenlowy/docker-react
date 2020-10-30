2e60a9fe651bb7a264269376963a59144f1b040f
Ellen Lowy <eslowy@att.net>
2020-10-30 09:06:03 +0000

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Simon and Alan
+          I love featuring cats!
         </p>
         <a
           className="App-link"
